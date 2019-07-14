0f218dd657c1f1f2c2a8c38ece5a7aae8b5b776c
Heather deManbey <h.demanbey@gmail.com>
2019-07-14 19:14:17 -0400

    weather app functional

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,43 +1,98 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
-//Define paths for Express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
-        name: 'Heather'
+        title: 'Weather',
+        name: 'Heather Demanbey'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About',
-        name: 'Heather'
+        title: 'About Me',
+        name: 'Heather Demanbey'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is where you would put help text....'
+        helpText: 'This is where you would put help text....',
+        title: 'Help',
+        name: 'Heather Demanbey'
     })
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error: error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: '50 degrees',
-        location: 'Philadelphia'
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Heather Demanbey',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Heather Demanbey',
+        errorMessage: 'Page not found.'
     })
 })
 
